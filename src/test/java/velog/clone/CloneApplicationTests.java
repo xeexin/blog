729c4aeabdc7c7620f8ce29647d833925ed96246
LIM HEE JIN <xeexin0331@gmail.com>
2024-06-18 13:46:17 +0900

    first commit

---
+++ src/test/java/velog/clone/CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package velog.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
